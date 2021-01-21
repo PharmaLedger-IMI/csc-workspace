042671c2449e0e98ba8428300fdb6f1bf7cf25c2
Nicole Blazier <nicole.blazier@novartis.com>
2021-01-20 21:56:08 -0600

    Adding PrivateSky files from ssapp

---
+++ node_modules/octopus/scripts/setEnv.js
@@ -0,0 +1,32 @@
+const argIdentifier = "--file=";
+const errorMessage = `Misuse of script. Syntax node path_to_script ${argIdentifier}'path_to_env_file' \"[npm cmd] [node cmd]\"`;
+const args = process.argv;
+args.splice(0, 2);
+
+const octopus = require("./index");
+if(args.length <2){
+	//throw new Error("This script expects exactly one argument as path to a JSON file that contains env variables that need to be set up.");
+	octopus.handleError(errorMessage);
+}
+
+let fileArg = args.shift();
+if(fileArg.indexOf(argIdentifier) === -1){
+	octopus.handleError(errorMessage);
+}
+fileArg = fileArg.replace(argIdentifier, "");
+
+let envJson;
+try{
+	envJson = require(fileArg);
+}catch(err){
+	octopus.handleError("env file not found or contains an invalid JSON!");
+}
+
+const {spawn} = require("child_process");
+Object.assign(process.env, envJson);
+console.log("Environment updated accordingly to env file passed as argument.");
+
+const spawn_cmd = args.join(" ");
+
+console.log("Preparing to execute cmd", spawn_cmd);
+spawn(spawn_cmd, undefined, {shell: true, stdio: "inherit"});
