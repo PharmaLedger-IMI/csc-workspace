34f108afbb8973e072356b1712568e513ae14968
MFreidank <freidankm@gmail.com>
2021-01-27 14:36:11 +0100

    build script - fantastic!

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+SERVER_PORT=8080
+kill $(lsof -ti:${SERVER_PORT})
+npm run server & disown
+echo "wait for the server to start.."
+sleep 2
+echo "Build changes.."
+npm run build-all
+
+echo "Open incognito localhost window on port ${SERVER_PORT}.."
+open -na "Google Chrome" --args --incognito "http://localhost:${SERVER_PORT}"
